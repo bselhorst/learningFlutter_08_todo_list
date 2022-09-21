e8ac28d91059515f6f7303e35145e334328aa0a6
Bruno Selhorst <bselhorst88@gmail.com>
2022-09-21 18:31:00 -0500

    'atualizações'

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:async/async.dart';
+import 'dart:convert';
+import 'dart:collection';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,10 +16,122 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _lista = ["Teste", "Sei la", "Carro"];
+  List _lista = [];
+  Map<String, dynamic> _ultimaTarefaRemovida = Map();
+  TextEditingController _controllerTarefa = TextEditingController();
+
+  Future<File> _getFile() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+    return File("${diretorio.path}/dados.json");
+  }
+
+  _salvarArquivo() async {
+    var arquivo = await _getFile();
+
+    // Map<String, dynamic> tarefa = Map();
+    // tarefa["titulo"] = "Ir ao mercado";
+    // tarefa["realizada"] = false;
+    // _lista.add(tarefa);
+
+    String dados = json.encode(_lista);
+    arquivo.writeAsString(dados);
+  }
+
+  _lerArquivo() async {
+    try {
+      final arquivo = await _getFile();
+      return arquivo.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  _salvarTarefa() {
+    String textoDigitado = _controllerTarefa.text;
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = textoDigitado;
+    tarefa["realizada"] = false;
+    setState(() {
+      _lista.add(tarefa);
+    });
+    _salvarArquivo();
+    _controllerTarefa.text = "";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _lerArquivo().then((dados) {
+      setState(() {
+        _lista = json.decode(dados);
+      });
+    });
+  }
+
+  Widget criarItemLista(context, index) {
+    // final item = _lista[index]["titulo"];
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        //Recuperar ultimo item
+        _ultimaTarefaRemovida = _lista[index];
+
+        //Remove da lista
+        _lista.removeAt(index);
+        _salvarArquivo();
+
+        //Snackbar
+        final snackBar = SnackBar(
+          // backgroundColor: Colors.green,
+          content: Text("Tarefa Removida"),
+          duration: Duration(seconds: 5),
+          action: SnackBarAction(
+            label: "Desfazer",
+            onPressed: () {
+              setState(() {
+                _lista.insert(index, _ultimaTarefaRemovida);
+              });
+              _salvarArquivo();
+            },
+          ),
+        );
+
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
+      background: Container(
+        padding: EdgeInsets.all(16),
+        color: Colors.red,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Icon(
+              Icons.delete,
+            ),
+          ],
+        ),
+      ),
+      child: CheckboxListTile(
+        title: Text(_lista[index]['titulo']),
+        value: _lista[index]['realizada'],
+        onChanged: (valorAlterado) {
+          setState(() {
+            _lista[index]['realizada'] = valorAlterado;
+          });
+          _salvarArquivo();
+        },
+      ),
+      // onDismissed: (direction) {
+
+      // },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    _salvarArquivo();
+    // print(_lista.toString());
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista de tarefas"),
@@ -25,11 +143,7 @@ class _HomeState extends State<Home> {
             Expanded(
               child: ListView.builder(
                 itemCount: _lista.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_lista[index]),
-                  );
-                },
+                itemBuilder: criarItemLista,
               ),
             ),
           ],
@@ -45,10 +159,10 @@ class _HomeState extends State<Home> {
               return AlertDialog(
                 title: Text("Adicionar Tarefa"),
                 content: TextField(
+                  controller: _controllerTarefa,
                   decoration: InputDecoration(
                     labelText: "Digite sua tarefa",
                   ),
-                  onChanged: (text) {},
                 ),
                 actions: [
                   ElevatedButton(
@@ -57,6 +171,7 @@ class _HomeState extends State<Home> {
                   ),
                   ElevatedButton(
                     onPressed: () {
+                      _salvarTarefa();
                       Navigator.pop(context);
                     },
                     child: Text("Salvar"),
